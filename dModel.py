1f75793fab29ff9057652faf3c9e0506ba74de3e
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-07-03 00:19:44 +0800

    Update dModel.py

--- dModel.py
+++ dModel.py
@@ -28,7 +28,5 @@ class UserData(db.Model):
         return "<UserData('%s', '%s')>" % (self.KeyWord, self.Description)
 
 
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    manager.run()
