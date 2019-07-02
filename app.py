5eed5530c13b836b44a3e9ec26a3f58f4bd265ba
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-07-03 00:12:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import request, abort
+from flask import Flask, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,9 +12,30 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-from dModel import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ifvbkjtshpsxqj:4972b22ed367ed7346b0107d3c3e97db14fac1dde628cd6d7f08cf502c927ee1@ec2-50-16-197-244.compute-1.amazonaws.com:5432/d6tkud0mtknjov'
+	
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+class UserData(db.Model):
+    __tablename__ = 'UserData'
+	
+    Id = db.Column(db.Integer, primary_key=True)
+    KeyWord = db.Column(db.String(255))
+    Description = db.Column(db.String(255))
+
+    def __init__(self, KeyWord, Description):
+        self.KeyWord = KeyWord
+        self.Description = Description
+
+    def __repr__(self):
+        return "<UserData('%s', '%s')>" % (self.KeyWord, self.Description)
 
 line_bot_api = LineBotApi('HRWbC4w2S3J3JvFAQQkQnp4gxXVWtCwLWgrdanU72Y26+hwAoZvdiwhjyLPuIPdYLaqqy4ZDIC48EDGEo9FDp0VhS453OJfXEfFCwoFhZxhIFy6ESVLFr7fPuythQb4WA4gvEHkCjJ+yuMJDgzeR8gdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('4abb8726ea0ae9dc4a91154ce6fecb60')
