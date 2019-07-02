8816b00672c50c38ea4190016cd75243be40a021
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-07-03 01:56:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -65,6 +65,22 @@ def handle_message(event):
                 event.reply_token,
                 TextSendMessage(text=content))
             return 0
+    if lineMessage[0:2] == "刪除":
+        lineMes = lineMessage.split(';')
+        keymessage = lineMes[1]
+        if excludeWord(keymessage, event) == 1:
+            conn = psycopg2.connect(database="d6tkud0mtknjov", user="ifvbkjtshpsxqj", password="4972b22ed367ed7346b0107d3c3e97db14fac1dde628cd6d7f08cf502c927ee1", host="ec2-50-16-197-244.compute-1.amazonaws.com", port="5432")
+            for message in lineMes[2:]:
+                cur = conn.cursor()
+                sql = "DELETE FROM userdata WHERE (KeyWord='%s' AND Description='%s');"
+                cur.execute(sql, (keymessage, message))
+                conn.commit()
+            conn.close()
+            content = "我把這些垃圾給吃了"
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=content))
+            return 0
     else:
         message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token, message)
