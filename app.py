af707a2c760c1bdb278fae17d3d113c6048b3b8c
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-07-02 20:49:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,7 @@ def handle_message(event):
         message)
 
 
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
