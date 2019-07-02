0d99ba80f511325c9512c5e89861e7eee2604c7c
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-07-02 21:37:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='Hello World'))
 
 
 if __name__ == "__main__":
