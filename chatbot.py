df4eaf660749294e3eb5daa8881a8daab287aac3
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-07-03 16:31:00 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -12,7 +12,7 @@ class LineChatBOT:
 
     def __init__(self):
         trainer = ChatterBotCorpusTrainer(self.chatbot)
-        trainer.train("chatterbot.corpus.traditionalChinese")
+        trainer.train("chatterbot_corpus/data/traditionalchinese")
 
     def getResponse(self, message=""):
         response = self.chatbot.get_response(message)
