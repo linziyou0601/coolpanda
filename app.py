fa964b8e8d6abd2f69b131b64c4e238ed884d0b9
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-07-03 22:45:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -241,7 +241,7 @@ def callback():
 @handler.add(FollowEvent)
 def handle_follow(event):
     profile = line_bot_api.get_profile(event.source.user_id)
-    content = TextSendMessage(text=profile.display_name + "，歡迎您成為本熊貓的好友" + u'0x100097')
+    content = TextSendMessage(text=profile.display_name + "，歡迎您成為本熊貓的好友" + u'0x100097'.encode("UTF-8"))
     message = FlexSendMessage(alt_text="hello", contents=msgFunc("main"))
     line_bot_api.reply_message(
         event.reply_token,
