b0bf9b9bb44b65e3ff8fb8e4af7dcab711678401
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-08-26 16:14:38 +0800

    update

--- cowpiDB.py
+++ cowpiDB.py
@@ -80,8 +80,7 @@ def adjustPrio(msg, case):
         c.execute('SELECT keyword, response, priority FROM statements Where response=?', [msg])
         data = c.fetchall()
         for x in data:
-            num=int(x[2])+int(case)
-            c.execute('UPDATE statements SET priority=? Where keyword=? and response=?', [num, x[0], [1]])
+            c.execute('UPDATE statements SET priority=? Where keyword=? and response=?', [int(x[2])+case, x[0], x[1]])
 def resStatement(key):
     createTable()
     with sqlite3.connect('db/cowpi.db') as conn:
