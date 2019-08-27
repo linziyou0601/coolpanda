59fa1d8646e2b8c22f746077cf8d7a6689fbbb3c
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-08-27 11:22:25 +0800

    update

--- manage/manage/wsgi.py
+++ manage/manage/wsgi.py
@@ -7,19 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-""" import os
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manage.settings')
 
-application = get_wsgi_application() """
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "專案名稱.settings")
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
