bd9ea3403f398e4d1bf35c1d895d08ad56623a98
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-08-27 10:05:13 +0800

    update

--- manage/manage.py
+++ manage/manage.py
@@ -19,6 +19,6 @@ def main():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
     main()
