8972a5bbb66187d2245acf6e54020a5a58e59084
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-08-27 23:41:02 +0800

    update

--- linechatbot/settings.py
+++ linechatbot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iojo_jy@=mnn_6)#=tdyv1*ayyw-97s5n2i6od6*%xbzvb^+2u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['linechatbot0.herokuapp.com','127.0.0.1']
 
