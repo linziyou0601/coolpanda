3c10e004925e3330f0d20531968896af758b1130
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-08-27 15:24:54 +0800

    update

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,56 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Received(models.Model):
+    message = models.TextField()
+    channel_id = models.TextField()
+    create_at = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'received'
+        app_label = 'bot'
+
+
+class Reply(models.Model):
+    message = models.TextField()
+    valid = models.IntegerField(blank=True, null=True)
+    channel_id = models.TextField()
+    create_at = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'reply'
+        app_label = 'bot'
+
+
+class Statements(models.Model):
+    keyword = models.TextField()
+    response = models.TextField()
+    create_at = models.TextField()
+    channel_id = models.TextField()
+    channel_type = models.TextField()
+    priority = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'statements'
+        app_label = 'bot'
+
+
+class Users(models.Model):
+    channel_id = models.TextField()
+    globaltalk = models.IntegerField()
+    mute = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'users'
+        app_label = 'bot'
