4c56da97115fe5cd3f2471ca6d80f3a342cdb851
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-08-30 15:08:57 +0800

    update

--- bot/cowpiDB.py
+++ bot/cowpiDB.py
@@ -57,33 +57,33 @@ def newChannel(channelId):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('SELECT * FROM users Where channel_id=?;', [channelId])
+    c.execute('SELECT * FROM users Where channel_id=%s;', [channelId])
     #若頻道資料不存在才建立
     if len(c.fetchall())==0:
-        c.execute('INSERT INTO users(channel_id) VALUES(?);', [channelId])
+        c.execute('INSERT INTO users(channel_id) VALUES(%s);', [channelId])
 ##刪除頻道資料
 def delChannel(channelId):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('DELETE FROM users Where channel_id=?;', [channelId])
+    c.execute('DELETE FROM users Where channel_id=%s;', [channelId])
 ##修改頻道功能
 def editChannelGlobalTalk(channelId, value):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('UPDATE users SET globaltalk=? Where channel_id=?;', [value, channelId])
+    c.execute('UPDATE users SET globaltalk=%s Where channel_id=%s;', [value, channelId])
 def editChannelMute(channelId, value):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('UPDATE users SET mute=? Where channel_id=?;', [value, channelId])
+    c.execute('UPDATE users SET mute=%s Where channel_id=%s;', [value, channelId])
 ##查詢頻道功能狀態
 def queryUser(channelId):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('SELECT * FROM users Where channel_id=?;', [channelId])
+    c.execute('SELECT * FROM users Where channel_id=%s;', [channelId])
     data = c.fetchall()
     return data[0] if len(data) else []
 
@@ -94,19 +94,19 @@ def storeReceived(msg, channelId):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('INSERT INTO received(message, channel_id, create_at) VALUES(?,?,?);', [msg, channelId, str(datetime.now(pytz.timezone("Asia/Taipei")))])
+    c.execute('INSERT INTO received(message, channel_id, create_at) VALUES(%s,%s,%s);', [msg, channelId, str(datetime.now(pytz.timezone("Asia/Taipei")))])
 ##儲存機器人回覆
 def storeReply(msg, valid, channelId):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('INSERT INTO reply(message, valid, channel_id, create_at) VALUES(?,?,?,?);', [msg, valid, channelId, str(datetime.now(pytz.timezone("Asia/Taipei")))])
+    c.execute('INSERT INTO reply(message, valid, channel_id, create_at) VALUES(%s,%s,%s,%s);', [msg, valid, channelId, str(datetime.now(pytz.timezone("Asia/Taipei")))])
 ##查詢收到的訊息
 def queryReceived(channelId, num):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('SELECT message FROM received Where channel_id=? ORDER BY id DESC limit ?;', [channelId, num])
+    c.execute('SELECT message FROM received Where channel_id=%s ORDER BY id DESC limit %s;', [channelId, num])
     data = c.fetchall()
     return [x[0] for x in data] if len(data) else [""]
 ##查詢機器人回覆
@@ -114,7 +114,7 @@ def queryReply(channelId, num):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('SELECT message, valid FROM reply Where channel_id=? ORDER BY id DESC limit ?;', [channelId, num])
+    c.execute('SELECT message, valid FROM reply Where channel_id=%s ORDER BY id DESC limit %s;', [channelId, num])
     data = c.fetchall()
     return [[x[0],x[1]] for x in data] if len(data) else [["",0]]
 
@@ -126,10 +126,10 @@ def insStatement(key, msg, channelId, type):
     conn = getConnect()
     c = conn.cursor()
     for res in msg:
-        c.execute('SELECT * FROM statements Where keyword=? and response=? and channel_id=?;', [key, res, channelId])
+        c.execute('SELECT * FROM statements Where keyword=%s and response=%s and channel_id=%s;', [key, res, channelId])
         #若詞條不存在於當前聊天室，才新增詞條
         if not len(c.fetchall()):
-            c.execute('INSERT INTO statements(keyword, response, create_at, channel_id, channel_type) VALUES(?,?,?,?,?);',
+            c.execute('INSERT INTO statements(keyword, response, create_at, channel_id, channel_type) VALUES(%s,%s,%s,%s,%s);',
                         [key, res, str(datetime.now(pytz.timezone("Asia/Taipei"))), channelId, type])
         #若詞條存在於當前聊天室，則權重+1
         else: adjustPrio(key, res, 1, channelId)  
@@ -139,7 +139,7 @@ def delStatement(key, msg, channelId):
     conn = getConnect()
     c = conn.cursor()
     for res in msg:
-        c.execute('DELETE FROM statements Where keyword=? and response=? and channel_id=?;', [key, res, channelId])
+        c.execute('DELETE FROM statements Where keyword=%s and response=%s and channel_id=%s;', [key, res, channelId])
 ##調整詞條權重
 def adjustPrio(key, msg, case, channelId=''):
     createTable()
@@ -150,12 +150,12 @@ def adjustPrio(key, msg, case, channelId=''):
     strChannelId = ''
     #若有指定channelId（手動加詞學習），則加入channelId條件；若為降低優先度，則不論keyword全一起降
     if case>0:
-        strKeyword = ' and keyword=?'
+        strKeyword = ' and keyword=%s'
         listN.append(key)
     if channelId:
-        strChannelId = ' and channel_id=?'
+        strChannelId = ' and channel_id=%s'
         listN.append(channelId)
-    c.execute('SELECT priority FROM statements Where response=?;' +strKeyword +strChannelId, listN)
+    c.execute('SELECT priority FROM statements Where response=%s;' +strKeyword +strChannelId, listN)
     data = c.fetchall()
     #若詞條找不到，表示此句為自動接話模型、或廣泛搜尋模型，則增加一句自動學習詞條
     if len(data):
@@ -168,14 +168,14 @@ def resStatement(key, channelId, rand):
     conn = getConnect()
     c = conn.cursor()
     #若關閉可以說其他人教過的話的功能，則以限制channelId的方式查詢
-    strGlobaltalk = 'likestrong>' if queryUser(channelId)[2] else 'channel_id=? and likestrong>'
+    strGlobaltalk = 'likestrong>' if queryUser(channelId)[2] else 'channel_id=%s and likestrong>'
     strRandomreply = '0 and priority>=5 ORDER BY RANDOM() limit 1;' if rand else '1 ORDER BY likestrong DESC, priority DESC, id DESC limit 1;'
     c.execute('''
         SELECT  response,
                 CASE
-                    WHEN keyword = ? THEN 3 
-                    WHEN keyword LIKE ? THEN 2
-                    WHEN keyword LIKE ? THEN 1
+                    WHEN keyword = %s THEN 3 
+                    WHEN keyword LIKE %s THEN 2
+                    WHEN keyword LIKE %s THEN 1
                     ELSE 0 
                 END as likestrong
         FROM statements Where ''' + strGlobaltalk + strRandomreply, 
@@ -187,9 +187,9 @@ def resStatement(key, channelId, rand):
         c.execute('''
             SELECT  response,
                     CASE
-                        WHEN keyword = ? THEN 3 
-                        WHEN keyword LIKE ? THEN 2
-                        WHEN keyword LIKE ? THEN 1
+                        WHEN keyword = %s THEN 3 
+                        WHEN keyword LIKE %s THEN 2
+                        WHEN keyword LIKE %s THEN 1
                         ELSE 0 
                     END as likestrong
             FROM statements Where channel_id='cowpi' and likestrong>0 ORDER BY RANDOM() limit 1;''', 
@@ -202,7 +202,7 @@ def allStatement(channelId):
     createTable()
     conn = getConnect()
     c = conn.cursor()
-    c.execute('SELECT keyword, response FROM statements Where channel_id=? ORDER BY keyword;', [channelId])
+    c.execute('SELECT keyword, response FROM statements Where channel_id=%s ORDER BY keyword;', [channelId])
     data = c.fetchall()
     #建立回傳物件
     status = queryUser(channelId)
@@ -240,6 +240,6 @@ def autoIfEmptyStatements():
             ['66666', '遛遛遛遛遛狗'],['哈哈', '哈哈哈哈哈密瓜'],['生氣', '厚氣氣氣氣氣'],['QQ', '不哭不哭你是豬'],['謝謝', '不客氣']
         ]
         for x in data:
-            c.execute('INSERT INTO statements(keyword, response, create_at, channel_id, channel_type, priority) VALUES(?,?,?,?,?,?);',
+            c.execute('INSERT INTO statements(keyword, response, create_at, channel_id, channel_type, priority) VALUES(%s,%s,%s,%s,%s,%s);',
             [x[0], x[1], str(datetime.now(pytz.timezone("Asia/Taipei"))), 'cowpi', 'autoLearn', 10])
 
